62a754b29e514a1ebf58d4743c5d89412e89ea35
Valdas Urbonas <urbonas.val@gmail.com>
2021-11-06 17:51:07 +0200

    more pythonic way script

--- cassandra_1.py
+++ cassandra_1.py
@@ -1,19 +1,28 @@
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
 
-
-auth_provider = PlainTextAuthProvider(username='valdas', password='Valdas123456')
-cluster = Cluster(['20.113.59.203'], auth_provider=auth_provider)
-session = cluster.connect()
-
 # preson_1 postcodes > 50 
 # person_2 postcodes <= 50 
 my_keyspaces = ['person_1', 'person_2']
 
 
-session.execute('USE {}'.format(my_keyspaces[0]))
+def connect_valdas():
+    auth_provider = PlainTextAuthProvider(username='valdas', password='Valdas123456')
+    cluster = Cluster(['20.113.59.203'], auth_provider=auth_provider)
+    session = cluster.connect()
 
-users = session.execute('SELECT * FROM recommendation')
+    return cluster, session
 
-for row in users:
-    print(row)
+def test_run(session):
+    session.execute('USE {}'.format(my_keyspaces[0]))
+
+    users = session.execute('SELECT * FROM recommendation')
+
+    for row in users:
+        print(row)
+
+
+if __name__ == "__main__":
+    cluster, session = connect_valdas()
+
+    test_run(session)
