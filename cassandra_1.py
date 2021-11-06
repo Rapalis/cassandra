fe05d9665b0df5b1d28ef6cf3b16ee5d788f65f7
dmkrap <radomas2@gmail.com>
2021-11-06 18:55:46 +0200

    Init dominykas db

--- cassandra_1.py
+++ cassandra_1.py
@@ -5,40 +5,37 @@ from cassandra.auth import PlainTextAuthProvider
 # preson_1 postcodes > 50 
 # person_2 postcodes <= 50 
 my_keyspaces = ['person_1', 'person_2']
+dominykas_keyspaces = ['a50', 'b50']
 
 cluster = None
 session = None
 
-def connect_valdas():
-    global cluster
-    global session
-
-    auth_provider = PlainTextAuthProvider(username='valdas', password='Valdas123456')
-    cluster = Cluster(['20.113.59.203'], auth_provider=auth_provider)
+def connectToServer(username, password, ipAddress):
+    auth_provider = PlainTextAuthProvider(username = username, password = password)
+    cluster = Cluster([ipAddress], auth_provider=auth_provider)
+    
     session = cluster.connect()
+    return [cluster, session]
 
-def test_run():
-    global session
-
+def test_run(session):
     users = session.execute('SELECT * FROM person')
 
     for row in users:
         print(row)
 
 
-def change_keyspace(index: int):
-    global session
-
-    session.execute('USE {}'.format(my_keyspaces[index]))
+def change_keyspace(session, keyspace):
+    session.execute(f'USE {keyspace}')
 
 
 if __name__ == "__main__":
-    connect_valdas()
+    #cluster, session] = connectToServer('valdas', 'Valdas123456', '20.113.59.203')
+    [dominykasCluster, dominykasSession] = connectToServer('domrap', 'Password123*', '13.74.59.114')
 
-    change_keyspace(0)
+    change_keyspace(dominykasSession, dominykas_keyspaces[0])
 
-    test_run()
+    test_run(dominykasSession)
 
-    change_keyspace(1)
+    change_keyspace(dominykasSession, dominykas_keyspaces[1])
 
-    test_run()
+    test_run(dominykasSession)
